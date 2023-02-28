1b865694d7d47cdb9fbd3047ad832a06000ced91
José Antonio Pinto dos Santos <jose.santos@ps.otg.com.br>
2023-02-28 14:45:27 -0300

    refactor: remove unused folders

--- RestClient_test.go
+++ RestClient_test.go
@@ -1,14 +1,13 @@
-package test
+package restclient
 
 import (
-	restclient2 "rest-client-api/infra/restclient"
 	"testing"
 )
 
 func TestRestClientGet(t *testing.T) {
-	authorization := restclient2.NewBasic("your_username", "")
-	httpClient := restclient2.NewRestClient()
-	_, err := httpClient.Get().
+	authorization := NewBasic("your_username", "")
+	httpClient := NewRestClient()
+	_, err := httpClient.Delete().
 		Url("https://api.pagar.me/core/v5/customers/customer_id/cards/card_id").
 		AddHeader("accept", "application/json").
 		AddHeader("content-type", "application/json").
