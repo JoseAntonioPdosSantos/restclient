8ef61d26e2db19852854983b268e04bccadee867
Jose Antonio Pinto dos Santos <joseantonio.dev@gmail.com>
2024-04-18 04:33:23 -0400

    feat: add formencoded content type to the project

--- content_type.go
+++ content_type.go
@@ -5,4 +5,5 @@ type ContentType string
 const ContentTypeDescription = "Content-Type"
 const (
 	ApplicationJson ContentType = "application/json"
+	FormEncoded     ContentType = "application/x-www-form-urlencoded"
 )
